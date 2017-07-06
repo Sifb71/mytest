8d80b962cc38e1ed98c28d24be0166fa61ca495a
sifb71 <feyzbakksh.s.iman@gmail.com>
2017-07-06 07:08:36 -0700

    resolve_paginate_address

--- WebApp/models/databases/mytest/table_models.py
+++ WebApp/models/databases/mytest/table_models.py
@@ -25,10 +25,10 @@ class PeeweeBaseModel(Model):
 
 class User(PeeweeBaseModel):
     id = PrimaryKeyField()
-    email = CharField()
-    password = CharField()
-    first_name = CharField()
-    last_name = CharField()
+    email = CharField(max_length=255)
+    password = CharField(max_length=255)
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
     status = CharField()  # user status filed contain this value: active, de_active, deleted
     created_at = DateTimeField(default=datetime.datetime.now())
 
@@ -37,11 +37,11 @@ class User_address(PeeweeBaseModel):
     id = PrimaryKeyField()
     address = TextField()
     user = ForeignKeyField(User, to_field=User.id, related_name='user_address')
-    street_number = CharField()
-    route = CharField()
-    locality = CharField()
-    street_number2 = CharField()
-    country = CharField()
+    street_number = CharField(max_length=100)
+    route = CharField(max_length=100)
+    locality = CharField(max_length=100)
+    street_number2 = CharField(max_length=100)
+    country = CharField(max_length=100)
     created_at = DateTimeField(default=datetime.datetime.now())
     is_validate = IntegerField(1)
 
